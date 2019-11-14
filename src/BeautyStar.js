53d983173a0ad9547d7873e11120b1e4bf2972a3
tangweikun <819573105@qq.com>
2019-11-14 17:38:04 +0800

    :sparkles: add BeautyStar

---
+++ src/BeautyStar.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Star } from "./Star";
+
+export function BeautyStar(props) {
+  const { maxStars = 5 } = props;
+  const [num, setNum] = useState(-1);
+
+  return (
+    <div
+      style={{
+        color: " #121621",
+        display: "flex"
+      }}
+    >
+      {Array(maxStars)
+        .fill(null)
+        .map((_, index) => (
+          <div
+            title={`${index} star`}
+            key={index}
+            onClick={() => {
+              setNum(index === num ? -1 : index);
+            }}
+            style={{
+              cursor: "pointer",
+              position: "relative",
+              marginRight: index !== maxStars ? 16 : 0
+            }}
+          >
+            <Star selected={index <= num} />
+          </div>
+        ))}
+    </div>
+  );
+}
