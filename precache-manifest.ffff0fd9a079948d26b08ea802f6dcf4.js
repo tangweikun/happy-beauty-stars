029ba4849994225c829f71c00eefa86906f7252f
tangweikun <819573105@qq.com>
2019-11-15 12:30:12 +0800

    Updates

---
+++ precache-manifest.ffff0fd9a079948d26b08ea802f6dcf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f790060edbdc5db3ed2e587916383fbe",
+    "url": "/happy-beauty-stars/index.html"
+  },
+  {
+    "revision": "19543e54fe5463b78ac0",
+    "url": "/happy-beauty-stars/static/css/main.7a4f7d6e.chunk.css"
+  },
+  {
+    "revision": "0fc1816b66fb6c21f871",
+    "url": "/happy-beauty-stars/static/js/2.f44810cf.chunk.js"
+  },
+  {
+    "revision": "19543e54fe5463b78ac0",
+    "url": "/happy-beauty-stars/static/js/main.1e885c7d.chunk.js"
+  },
+  {
+    "revision": "15251aff4fb494ff348d",
+    "url": "/happy-beauty-stars/static/js/runtime-main.1a01e658.js"
+  }
+]);
